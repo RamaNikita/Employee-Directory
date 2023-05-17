023bbf961f1f8f04853c344ce61414187b3acf63
Nikita_Rama <nikita.rama10@gmail.com>
2023-05-17 10:48:08 -0500

    new

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./style.css";
 import Homepage from "./components/Homepage";
 import EmployeePage from "./components/EmployeePage";
 export default function App() {
-  const arr = [
-    {
-      name: "James King",
-      jobTitle: "President and CEO",
-      Office: "781-000-0002",
-      sms: "781-000-0002",
-      mobile: "781-000-0002",
-      email: "jking@fakemail.com",
-    },
-  ];
+  // const arr = [
+  //   {
+  //     name: "James King",
+  //     jobTitle: "President and CEO",
+  //     Office: "781-000-0002",
+  //     sms: "781-000-0002",
+  //     mobile: "781-000-0002",
+  //     email: "jking@fakemail.com",
+  //   },
+  // ];
   return (
     <div className="app">
       <Homepage text="Employee Directory" />
